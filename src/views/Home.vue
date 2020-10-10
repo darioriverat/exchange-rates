2d040660a3b3c7dc6b5dee06ab22e29ea3d38e1d
Darío Rivera <fermius.us@gmail.com>
2020-10-10 18:56:22 -0500

    feat: add last exchange rate in home from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,59 @@
 <template>
-  <h1>Home</h1>
+  <div class="container">
+    <h1 class="text-center mb-5">Exchange Rates Today</h1>
+    <div class="row">
+      <div class="col-sm-4 mb-2">
+        <div class="card" style="width: 18rem;">
+          <h1 class="currency text-center">{{ currency }}</h1>
+          <div class="card-body">
+            <p class="card-text text-center">
+              {{ date }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-8">
+        <div class="row">
+          <div
+            class="col-4 col-sm-3 col-md-2 text-center mb-2"
+            v-for="(value, rate) in rates"
+            :key="value"
+          >
+            {{ rate }} <br />
+            {{ value }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currency: "",
+      date: "",
+      rates: {},
+    };
+  },
+  created() {
+    this.$http.get("https://api.ratesapi.io/api/latest").then((res) => {
+      console.info(res);
+      this.currency = res.body.base;
+      this.date = res.body.date;
+      this.rates = res.body.rates;
+    });
+  },
+};
+</script>
+
+<style scoped>
+.currency {
+  font-size: 3rem;
+  padding: 40px 0;
+}
+.card {
+  width: 100% !important;
+}
+</style>
